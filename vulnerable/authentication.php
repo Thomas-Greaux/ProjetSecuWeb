6e42707b52b0cc99b3b16739eaf872d13eaa3600
Thomas Greaux <thomas.greaux@etu.unice.fr>
2018-10-18 11:34:15 +0200

    simple logging

---
+++ vulnerable/authentication.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Authentication.php</h1>
+
+<p>Login successful. Hello <?php echo $_POST["username"]; ?> with password <?php echo $_POST["password"]; ?> </p>
+
+</body>
+</html>
