e1de62034e320484b0467c9d9418d5f6e049d430
sebastien <sebastien.degand@etu.unice.fr>
2018-10-20 17:28:22 +0200

    login with db

--- vulnerable/authentication.php
+++ vulnerable/authentication.php
@@ -1,10 +1,37 @@
+<?php 
+if($_POST["username"] && $_POST["password"]){
+	$servernamedb = "127.0.0.1";
+	$usernamedb = "secu";
+	$passworddb = "secu";
+	$dbname = "secudb";
+	// Create connection
+	$conn = new mysqli($servernamedb, $usernamedb, $passworddb, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+   		die("Connection failed: " . $conn->connect_error);
+	}
+	// request with username and password
+	$sql = "SELECT * FROM user where username='".$_POST["username"]."' and password='".$_POST["password"]."'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0){
+		$row = $result->fetch_assoc();
+		setcookie('username', serialize($row["username"]), time()+3600);
+		setcookie('password', serialize($row["password"]), time()+3600);	
+	}
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
 
 <h1>Authentication.php</h1>
-
-<p>Login successful. Hello <?php echo $_POST["username"]; ?> with password <?php echo $_POST["password"]; ?> </p>
-
+<?php
+	if ($result->num_rows > 0){
+		echo "<p>Login successful. Hello ".$row["username"]." with password ".$row["password"]."</p>";
+	} else {
+			echo "<p>Login failed</p>";
+		}
+?>
 </body>
 </html>
