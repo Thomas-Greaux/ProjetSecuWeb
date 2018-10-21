3053a9c4a0a916f87dafaf3c1a03d57a7713fbe0
sebastien <sebastien.degand@etu.unice.fr>
2018-10-21 18:41:24 +0200

    add draft functionnality with cookie in guestbook

--- vulnerable/livre_d_or.php
+++ vulnerable/livre_d_or.php
@@ -23,7 +23,7 @@ $messages=file_get_contents($file);
 
 <head>
  <script>
-   function save(){ 
+    function save(){ 
         var callback = function () {     
         var show=document.getElementById("show"); 
 	show.innerHTML= xmlhttp.responseText;       
@@ -33,16 +33,24 @@ $messages=file_get_contents($file);
      var xmlhttp = new XMLHttpRequest();	    
      xmlhttp.open('GET',url, true);
      xmlhttp.onreadystatechange = callback;
-     xmlhttp.send(null);      
- } 
-  
+     xmlhttp.send(null); 
+     document.cookie = "draft_mesage" + "=" + "; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";     
+    }
+
+    function saveCurrentDraftInput() {
+        var value = document.getElementById("message").value
+        var d = new Date();
+        d.setTime(d.getTime() + (1*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = "draft_mesage" + "=" + value + ";" + expires + ";path=/";
+    }
 
  </script>
 
 </head>
 <body>
 <h1>Welcome to our Guest Book, Leave us a Message! </h1>
-<input  id="message" >
+<input  id="message" onchange= 'saveCurrentDraftInput()' value=<?php echo "'".$_COOKIE["draft_mesage"]."'"; ?> >
 <button onclick= 'save()' >Leave a message</button> 
 <h2>All the messages left by guests </h2>
 <div id="show"><?php echo $messages?> </div>
